cae7f7b85aa89027be85b5b6b90396dcfd515212
tysons2001 <tysons_2001@hotmail.com>
2025-12-19 18:18:20 +1100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,39 +1,5 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
-}
-
-android {
-  namespace = "com.tyson.fishinglogbook"
-  compileSdk = 35
-
-  defaultConfig {
-    applicationId = "com.tyson.fishinglogbook"
-    minSdk = 26
-    targetSdk = 35
-    versionCode = 2
-    versionName = "1.1"
-  }
-
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions { jvmTarget = "17" }
-
-  buildFeatures { compose = true }
-  composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
-
-  packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-}
-
-dependencies {
-  implementation(platform("androidx.compose:compose-bom:2024.10.00"))
-  implementation("androidx.activity:activity-compose:1.9.3")
-
-  implementation("androidx.compose.ui:ui")
-  implementation("androidx.compose.ui:ui-tooling-preview")
-  implementation("androidx.compose.material3:material3:1.3.1")
-
-  debugImplementation("androidx.compose.ui:ui-tooling")
+  id("org.jetbrains.kotlin.plugin.compose")
 }
