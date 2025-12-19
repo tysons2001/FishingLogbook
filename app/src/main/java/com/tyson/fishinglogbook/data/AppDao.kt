eb3d39be1fdd3b316d8db26ae6340dc3baa576b9
tysons2001 <tysons_2001@hotmail.com>
2025-12-19 18:54:47 +1100

    Create AppDao.kt

---
+++ app/src/main/java/com/tyson/fishinglogbook/data/AppDao.kt
@@ -0,0 +1,28 @@
+package com.tyson.fishinglogbook.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AppDao {
+
+    @Query("SELECT * FROM trips ORDER BY startMillis DESC")
+    fun observeTrips(): Flow<List<TripEntity>>
+
+    @Query("SELECT * FROM trips WHERE endMillis IS NULL ORDER BY startMillis DESC LIMIT 1")
+    fun observeActiveTrip(): Flow<TripEntity?>
+
+    @Insert
+    suspend fun insertTrip(item: TripEntity): Long
+
+    @Query("UPDATE trips SET endMillis = :endMillis WHERE id = :id")
+    suspend fun endTrip(id: Long, endMillis: Long)
+
+    @Query("SELECT * FROM catches ORDER BY timestampMillis DESC")
+    fun observeAllCatches(): Flow<List<CatchEntity>>
+
+    @Insert
+    suspend fun insertCatch(item: CatchEntity): Long
+}
