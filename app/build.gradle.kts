0c01836f6d85f65cb488c5db3cf1cdb490629038
tysons2001 <tysons_2001@hotmail.com>
2025-12-19 17:56:40 +1100

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+android {
+  namespace = "com.tyson.fishinglogbook"
+  compileSdk = 35
+
+  defaultConfig {
+    applicationId = "com.tyson.fishinglogbook"
+    minSdk = 26
+    targetSdk = 35
+    versionCode = 1
+    versionName = "1.0"
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+  implementation("androidx.core:core-ktx:1.15.0")
+  implementation("androidx.activity:activity-compose:1.9.3")
+  implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+  implementation("androidx.compose.material3:material3:1.3.1")
+}
