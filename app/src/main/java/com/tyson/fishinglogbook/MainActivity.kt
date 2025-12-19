48cf8926d2e58dbd4d61d3e7217e772b7e8159ca
tysons2001 <tysons_2001@hotmail.com>
2025-12-19 18:13:13 +1100

    Update MainActivity.kt

--- app/src/main/java/com/tyson/fishinglogbook/MainActivity.kt
+++ app/src/main/java/com/tyson/fishinglogbook/MainActivity.kt
@@ -3,11 +3,19 @@ package com.tyson.fishinglogbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { Text("Fishing Logbook installed ✅") }
+    setContent {
+      MaterialTheme {
+        Surface {
+          Text("Fishing Logbook installed ✅")
+        }
+      }
+    }
   }
 }
