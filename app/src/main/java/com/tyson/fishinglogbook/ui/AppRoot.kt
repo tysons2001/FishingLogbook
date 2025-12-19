c9ffb6e32a3fd894a942cc9b33d0084fbc674fbe
tysons2001 <tysons_2001@hotmail.com>
2025-12-19 20:42:02 +1100

    Update AppRoot.kt

--- app/src/main/java/com/tyson/fishinglogbook/ui/AppRoot.kt
+++ app/src/main/java/com/tyson/fishinglogbook/ui/AppRoot.kt
@@ -4,16 +4,26 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.tyson.fishinglogbook.ui.screens.AddTripScreen
 import com.tyson.fishinglogbook.ui.screens.HomeScreen
+import com.tyson.fishinglogbook.ui.screens.TripScreen
 
 @Composable
 fun AppRoot() {
     val nav = rememberNavController()
+
     NavHost(navController = nav, startDestination = "home") {
         composable("home") {
             HomeScreen(
-                onStartTrip = { nav.navigate("home") } // placeholder for next batch
+                onStartTrip = { nav.navigate("trip_add") },
+                onOpenActiveTrip = { nav.navigate("trip_active") }
             )
+        }
+        composable("trip_add") {
+            AddTripScreen(onDone = { nav.popBackStack() })
+        }
+        composable("trip_active") {
+            TripScreen(onBack = { nav.popBackStack() })
         }
     }
 }
